be29fa6b894242d1d4cd5faab338a8317e0f33b6
Lawisne2 <118357025+Lawisne2@users.noreply.github.com>
2023-05-10 16:43:07 +0900

    Update TrafficLightPublisher.cs

--- TrafficLightPublisher.cs
+++ TrafficLightPublisher.cs
@@ -29,6 +29,7 @@ namespace AWSIM
 
         IPublisher<std_msgs.msg.String> trafficLightPublisher;
         TrafficLight trafficlight;
+        float timer = 0;
 
         void Start()
         {
@@ -37,6 +38,14 @@ namespace AWSIM
 
         void Update()
         {
+            // Update every second
+            timer += Time.deltaTime;
+            var interval = 1.0f;        // 1 second
+            interval -= 0.00001f;       // Allow for accuracy errors.
+            if (timer < interval)
+                return;
+            timer = 0;
+
             trafficLightPublisher = SimulatorROS2Node.CreatePublisher<std_msgs.msg.String>(stateTopic);
             std_msgs.msg.String msg = new std_msgs.msg.String();
             AWSIM.TrafficLight.BulbData[] bulbDataArray = trafficlight.GetBulbData();
@@ -44,10 +53,17 @@ namespace AWSIM
             msg.Data = trafficlight.UniqueId.ToString() + " "; 
         
             //add the global coordinates
-            Vector3 globalPosition = trafficlight.transform.position + Environment.Instance.MgrsOffsetPosition;
-        
+            //Since the local referential is rotated 90 degrees, we need to swap the x and z coordinates
+
+            Vector3 globalPosition = trafficlight.transform.position;
+            globalPosition.z = Environment.Instance.MgrsOffsetPosition.z + globalPosition.y;
+            globalPosition.y = Environment.Instance.MgrsOffsetPosition.y + globalPosition.x;
+            globalPosition.x = Environment.Instance.MgrsOffsetPosition.x + globalPosition.z;
+
+            //sending the global position of the traffic light        
             msg.Data += globalPosition.ToString() + " ";
 
+            //sending the state of the traffic light
             for (int i = 0; i < bulbDataArray.Length; i++)
             {
                 msg.Data += bulbDataArray[i].Type.ToString() + " ";
